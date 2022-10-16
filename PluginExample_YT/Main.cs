55113feb497c42fb5ef405b95591b4ff83050c11
GrandpaRex <tomhenkel95@gmail.com>
2022-10-16 12:57:44 -0500

    Add project files.

---
+++ PluginExample_YT/Main.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using FivePD.API;
+using FivePD.API.Utils;
+
+#pragma warning disable 1998
+namespace PluginExample_YT
+{
+    public class Main : Plugin
+    {
+        internal Main()
+        {
+            _ = Command();
+        }
+
+        internal async Task Command()
+        {
+            RegisterCommand("minfo", new Action<int, List<object>, string>((source, args, rawCommand) =>
+                {
+                    //Get Player data
+                    PlayerData pdata = Utilities.GetPlayerData();
+
+                    //Assign all data to variables
+                    string badge = pdata.Callsign;
+                    string dept = pdata.DepartmentShortName;
+                    int deptid = pdata.DepartmentID;
+                    string deptln = pdata.Department;
+                    string rank = pdata.Rank;
+                    string pname = pdata.DisplayName;
+
+                    //Trigger chat message
+                    TriggerEvent("chatLaddMessage", new
+                    {
+                        color = new[] {178,190,181},
+                        args = new[] {$"{badge} {pname}, {rank} | {deptln} | {deptid} {dept}"}
+                    });
+
+                }), false);
+        }
+    }
+}
